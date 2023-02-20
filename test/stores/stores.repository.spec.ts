fd768a21cae6fcc2f87bbf34e2a831229ed35914
Guilherme Caramori <gcaramori16@gmail.com>
2023-02-20 08:42:10 -0300

    First test passed, creating a new store

---
+++ test/stores/stores.repository.spec.ts
@@ -0,0 +1,54 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { mockStore } from './stores';
+import { Store } from '../../src/stores/entities/store.entity';
+import { StoreRepository } from '../../src/stores/store.repository';
+
+describe('Store repository', () => {
+    let repository: StoreRepository;
+    let ormMock: Repository<Store>;
+
+    beforeEach(async () => {
+        const mockOrmRepository = {
+            create: jest.fn(),
+            save: jest.fn()
+        }
+
+        const moduleRef = await Test.createTestingModule({
+            providers: [
+                StoreRepository,
+                {
+                    provide: getRepositoryToken(Store),
+                    useValue: mockOrmRepository
+                }
+            ],
+        }).compile();
+        
+        repository = moduleRef.get<StoreRepository>(StoreRepository);
+        ormMock = moduleRef.get(getRepositoryToken(Store));
+    });
+
+    describe('creating a store', () => {
+        it('should create a new store', async () => {
+            jest.spyOn(ormMock, 'create').mockReturnValueOnce(mockStore())
+
+            const response = await repository.create({
+                name: 'random store',
+                cnpj: '55.674.472/0001-11',
+                address: 'Rua Olivio Belinate, 147',
+                telephone: '(19) 97858-3232',
+                qtyCars: 10,
+                qtyMotorcicles: 20
+            })
+
+            expect(response.id).toBeTruthy();
+            expect(response.name).toBe('random store');
+            expect(response.cnpj).toBe('55.674.472/0001-11');
+            expect(response.address).toBe('Rua Olivio Belinate, 147');
+            expect(response.telephone).toBe('(19) 97858-3232');
+            expect(response.qtyCars).toBe(10);
+            expect(response.qtyMotorcicles).toBe(20);
+        })
+    });
+})
