d27fd93de592204b3166c5d761aa13bed1432d6f
Guilherme Caramori <gcaramori16@gmail.com>
2023-02-21 07:58:51 -0300

    Created and tested store.service.spec, that is basically store.controller.spec but using store repository instead of store service

--- test/stores/store.service.spec.ts
+++ test/stores/store.service.spec.ts
@@ -1,11 +1,11 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { createStoreMock, updateStoreMock, mockStore } from "./stores";
-import { StoreController } from "../../src/stores/store.controller";
 import { StoreService } from "../../src/stores/store.service";
+import { StoreRepository } from "../../src/stores/store.repository";
 
-describe('Store controller', () => {
-    let controller: StoreController;
+describe('Store service', () => {
     let service: StoreService;
+    let repository: StoreRepository;
 
     beforeEach(async () => {
         const mockService = {
@@ -17,38 +17,38 @@ describe('Store controller', () => {
         }
 
         const module: TestingModule = await Test.createTestingModule({
-            controllers: [StoreController],
             providers: [
+                StoreService,
                 {
-                    provide: StoreService,
+                    provide: StoreRepository,
                     useValue: mockService
                 }
             ]
         }).compile();
 
-        controller = module.get<StoreController>(StoreController);
         service = module.get<StoreService>(StoreService);
+        repository = module.get<StoreRepository>(StoreRepository);
     });
 
     it('should be defined', () => {
-        expect(controller).toBeDefined();
+        expect(service).toBeDefined();
     });
 
     describe('create a store', () => {
-        it('should call Store service create with correct values', async () => {
+        it('should call Store repository create with correct values', async () => {
           const createSpy = jest.spyOn(service, 'create');
     
           const mockParam = createStoreMock();
     
-          await controller.create(mockParam);
+          await service.create(mockParam);
     
           expect(createSpy).toHaveBeenCalledWith(mockParam);
         });
 
-        it('should throw if Store service create throw errors', async () => {
+        it('should throw if Store repository create throw errors', async () => {
             jest.spyOn(service, 'create').mockRejectedValueOnce(new Error());
       
-            await expect(controller.create(createStoreMock())).rejects.toThrow(new Error());
+            await expect(service.create(createStoreMock())).rejects.toThrow(new Error());
         });
         
         it('should return a Store on success', async () => {
@@ -56,25 +56,25 @@ describe('Store controller', () => {
             
             jest.spyOn(service, 'create').mockResolvedValueOnce(mockReturn);
       
-            const response = await controller.create(createStoreMock())
+            const response = await service.create(createStoreMock())
       
             expect(response).toEqual(mockReturn);
         });
     });
 
     describe('find all stores', () => {
-        it('should call Store service find all', async () => {
+        it('should call Store repository find all', async () => {
             const findSpy = jest.spyOn(service, 'findAll');
 
-            await controller.findAll();
+            await service.findAll();
 
             expect(findSpy).toHaveBeenCalled();
         });
 
-        it('should throw if Store service find all throw errors', async () => {
+        it('should throw if Store repository find all throw errors', async () => {
             jest.spyOn(service, 'findAll').mockRejectedValueOnce(new Error());
 
-            await expect(controller.findAll()).rejects.toThrow(new Error());
+            await expect(service.findAll()).rejects.toThrow(new Error());
         });
 
         it('should return an array of Stores on success', async () => {
@@ -82,25 +82,25 @@ describe('Store controller', () => {
 
             jest.spyOn(service, 'findAll').mockResolvedValueOnce(mockReturn);
 
-            const response = await controller.findAll();
+            const response = await service.findAll();
 
             expect(response).toEqual(mockReturn);
         });
     });
 
     describe('find a store by id', () => {
-        it('should call Store service find by id correctly', async () => {
+        it('should call Store repository find by id correctly', async () => {
             const findSpy = jest.spyOn(service, 'findById');
 
-            await controller.findById('randomid');
+            await service.findById('randomid');
 
             expect(findSpy).toHaveBeenCalledWith('randomid');
         });
         
-        it('should throw if Store service find by id throw errors', async () => {
+        it('should throw if Store repository find by id throw errors', async () => {
             jest.spyOn(service, 'findById').mockRejectedValueOnce(new Error());
 
-            await expect(controller.findById('randomid')).rejects.toThrow(new Error());
+            await expect(service.findById('randomid')).rejects.toThrow(new Error());
         });
         
         it('should return a Store on success', async () => {
@@ -108,43 +108,43 @@ describe('Store controller', () => {
 
             jest.spyOn(service, 'findById').mockResolvedValueOnce(mockReturn);
 
-            const response = await controller.findById('randomid');
+            const response = await service.findById('randomid');
 
             expect(response).toEqual(mockReturn);
         });
     });
 
     describe('update a store', () => {
-        it('should call Store service update with correct id and values', async () => {
+        it('should call Store repository update with correct id and values', async () => {
             const updateSpy = jest.spyOn(service, 'update');
         
             const mockParam = updateStoreMock();
         
-            await controller.update('randomid', mockParam);
+            await service.update('randomid', mockParam);
         
             expect(updateSpy).toHaveBeenCalledWith('randomid', mockParam);
         });
 
-        it('should throw if Store service update throw errors', async () => {
+        it('should throw if Store repository update throw errors', async () => {
             jest.spyOn(service, 'update').mockRejectedValueOnce(new Error());
 
-            await expect(controller.update('randomid', updateStoreMock())).rejects.toThrow(new Error());
+            await expect(service.update('randomid', updateStoreMock())).rejects.toThrow(new Error());
         });
     });
 
     describe('delete a store', () => {
-        it('should call Store service delete with correct id', async () => {
+        it('should call Store repository delete with correct id', async () => {
             const deleteSpy = jest.spyOn(service, 'delete');
 
-            await controller.delete('randomid');
+            await service.delete('randomid');
 
             expect(deleteSpy).toHaveBeenCalledWith('randomid');
         });
 
-        it('should throw if Store service delete throw errors', async () => {
+        it('should throw if Store repository delete throw errors', async () => {
             jest.spyOn(service, 'delete').mockRejectedValueOnce(new Error());
 
-            await expect(controller.delete('randomid')).rejects.toThrow(new Error())
+            await expect(service.delete('randomid')).rejects.toThrow(new Error())
         });
     });
 });
